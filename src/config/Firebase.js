8fde0aa347aed21eb3d8c4bb8e82406df36db41b
Bruno Paula <brunopaula@dryserv.com>
2020-09-15 18:52:08 -0400

    changed react router route

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -2,16 +2,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-// var config = {
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: 'authentication-boilerpla-ef1f4.firebaseapp.com',
-//   databaseURL: 'https://authentication-boilerpla-ef1f4.firebaseio.com',
-//   projectId: 'authentication-boilerpla-ef1f4',
-//   storageBucket: 'authentication-boilerpla-ef1f4.appspot.com',
-//   messagingSenderId: '10190348624'
-// }
-
-const config1 = {
+const config = {
   apiKey: process.env.REACT_APP_APPID,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   databaseURL: process.env.REACT_APP_DATABASEURL,
@@ -20,7 +11,7 @@ const config1 = {
   messagingSendId: process.env.REACT_APP_MESSAGINGSENDERID,
 }
 
-firebase.initializeApp(config1)
+firebase.initializeApp(config)
 firebase.firestore()
 
 export default firebase
